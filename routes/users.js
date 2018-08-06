ce10c37f10a6fe27ce4bb15cddbd38fa9b7ef18b
sangwell@126.com <JJin@corp.mm-software.com>
2018-08-06 14:39:39 +0800

    Updata files

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
+
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root'
+});
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  var Id = req.body.Id;
+  var Title = req.body.Title;
+  var Content = req.body.Content;
+  var Date = req.body.Date;
+  var Tags = req.body.Tags;
+  var sqlUrl = 'call blog.blog_add(?, ?, ?, ?, ?);';
+  var inParams = [Id,Title,Content,Date,Tags];
+  connection.connect();
+  connection.query(sqlUrl, inParams, function(err, rows, fields) {
+      if (err) throw err;
+      res.json(rows);
+  });
+  connection.end();
 });
 
 module.exports = router;
